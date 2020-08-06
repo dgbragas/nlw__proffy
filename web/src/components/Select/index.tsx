09d59f90db64a86c2a62eac4955265b3fc53a756
dgbragas <dgbragas.dev@outlook.com>
2020-08-06 17:14:38 -0300

    Solving application warning

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -14,8 +14,8 @@ interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
 const Select: React.FC<SelectProps> = ({ name, label, options, ...rest }) => (
   <S.FormGroup>
     <label htmlFor={name}>{label}</label>
-    <select id={name} {...rest}>
-      <option value="" disabled selected hidden>
+    <select id={name} defaultValue="" {...rest}>
+      <option value="" disabled hidden>
         Selecione uma opção
       </option>
       {options.map(option => (
