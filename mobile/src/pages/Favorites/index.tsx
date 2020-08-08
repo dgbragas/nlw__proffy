0676e0ca160eb41c5a5dbf8074e3d83e032f2bb1
dgbragas <dgbragas.dev@outlook.com>
2020-08-08 18:58:39 -0300

    Importing <PageHeader /> in default page structures

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import * as S from './styles';
+import PageHeader from '../../components/PageHeader';
 
 const Favorites: React.FC = () => {
-  return <S.Container />;
+  return (
+    <S.Container>
+      <PageHeader title="Meus Proffys favoritos" />
+    </S.Container>
+  );
 };
 
 export default Favorites;
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
 import * as S from './styles';
 
 const TeacherList: React.FC = () => {
-  return <S.Container />;
+  return (
+    <S.Container>
+      <PageHeader title="Proffys disponíveis" />
+    </S.Container>
+  );
 };
 
 export default TeacherList;
