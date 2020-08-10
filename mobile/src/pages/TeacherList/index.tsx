36aec18bb383fda9da247a95bb4d25c76134d583
dgbragas <dgbragas.dev@outlook.com>
2020-08-09 23:16:01 -0300

    Initial integration in <TeacherList />

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,13 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Feather } from '@expo/vector-icons';
+import { BorderlessButton } from 'react-native-gesture-handler';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+
+import api from '../../services/api';
 
 import * as S from './styles';
 
 const TeacherList: React.FC = () => {
+  const [isFiltersVisibile, setIsFiltersVisibile] = useState(false);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  const [teachers, setTeachers] = useState([]);
+
+  function handleToggleFiltersVisibile() {
+    setIsFiltersVisibile(!isFiltersVisibile);
+  }
+
+  async function handleFiltersSubmit() {
+    try {
+      const { data } = await api.get('/classes', {
+        params: {
+          subject,
+          week_day,
+          time,
+        },
+      });
+
+      setTeachers(data);
+      setIsFiltersVisibile(false);
+    } catch {
+      console.error('Erro ao obter professores');
+    }
+  }
+
   return (
     <S.Container>
-      <PageHeader title="Proffys disponíveis" />
+      <PageHeader
+        title="Proffys disponíveis"
+        headerRight={
+          <BorderlessButton onPress={handleToggleFiltersVisibile}>
+            <Feather name="filter" size={20} color="#FFF" />
+          </BorderlessButton>
+        }
+      >
+        {isFiltersVisibile && (
+          <S.SearchForm>
+            <S.Label>Matéria</S.Label>
+            <S.Input
+              placeholder="Qual a matéria?"
+              value={subject}
+              onChangeText={text => setSubject(text)}
+            />
+
+            <S.InputGroup>
+              <S.InputBlock>
+                <S.Label>Dia da semana</S.Label>
+                <S.Input
+                  placeholder="Qual a dia?"
+                  value={week_day}
+                  onChangeText={text => setWeekDay(text)}
+                />
+              </S.InputBlock>
+
+              <S.InputBlock>
+                <S.Label>Horário</S.Label>
+                <S.Input
+                  placeholder="Qual o horário?"
+                  value={time}
+                  onChangeText={text => setTime(text)}
+                />
+              </S.InputBlock>
+            </S.InputGroup>
+
+            <S.SubmitButton onPress={handleFiltersSubmit}>
+              <S.SubmitButtonText>Filtrar</S.SubmitButtonText>
+            </S.SubmitButton>
+          </S.SearchForm>
+        )}
+      </PageHeader>
+
+      <S.TeacherList
+        contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
+      >
+        {teachers.map((teacher: Teacher) => (
+          <TeacherItem key={teacher.id} teacher={teacher} />
+        ))}
+      </S.TeacherList>
     </S.Container>
   );
 };
--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -1,6 +1,57 @@
 import styled from 'styled-components/native';
+import { ScrollView } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   background-color: #f0f0f7;
+`;
+
+export const SearchForm = styled.View`
+  margin-bottom: 24px;
+`;
+
+export const Label = styled.Text`
+  color: #d4c2ff;
+  font-family: 'Poppins400';
+`;
+
+export const Input = styled.TextInput.attrs({
+  paddingHorizontal: 16,
+  placeholderTextColor: '#c1bccc',
+})`
+  background-color: #fff;
+  border-radius: 8px;
+  justify-content: center;
+  height: 54px;
+  margin-top: 4px;
+  margin-bottom: 16px;
+`;
+
+export const InputGroup = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const InputBlock = styled.View`
+  width: 48%;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  background-color: #04d361;
+  align-items: center;
+  border-radius: 8px;
+  flex-direction: row;
+  height: 56px;
+  justify-content: center;
+`;
+
+export const SubmitButtonText = styled.Text`
+  color: #fff;
+  font-family: 'Archivo700';
+  font-size: 16px;
+`;
+
+export const TeacherList = styled(ScrollView)`
+  margin-top: -40px;
 `;
